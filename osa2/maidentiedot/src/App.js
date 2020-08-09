1e07490af286fdd6c8476e13e5b8eba1df5930ac
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2020-08-09 16:55:01 +0300

    Wheather is working

--- osa2/maidentiedot/src/App.js
+++ osa2/maidentiedot/src/App.js
@@ -25,6 +25,25 @@ const PrintLanguages = ({languages}) =>{
   </div>
   )
 }
+const PrintwheatherData = ({country,data}) =>{
+
+  console.log(data)
+  if(country!=undefined && data.current!=undefined){
+    
+    return(
+      <div>
+        <h1>Wheater in {country.capital}</h1>
+        <br></br>
+        Temp : {data.current.temp_c}
+        <br></br>
+        Condition : {data.current.condition.text}
+            </div>
+    )
+  }
+  else{
+    return null
+  }
+}
 const PrintSingleCountry  = ({country}) =>{
   return (
     <div>
@@ -38,21 +57,22 @@ const PrintSingleCountry  = ({country}) =>{
     </div>
   )
 }
-const PrintCountries = ({ countries, filterString }) => {
 
-  var filteredCountries = filterCountries(countries, filterString)
+const PrintCountries = ({ filteredCountries}) => {
   if(filteredCountries[0]!=undefined){
     if(filteredCountries.length===1){
       return(
+        <div>
       <PrintSingleCountry country={filteredCountries[0]}>
-
-      </PrintSingleCountry>)
+      </PrintSingleCountry>
+      </div>)
     }
+
     if(filteredCountries.length < 10){
       console.log("Less than 10")
       return (
         <div>
-          {filteredCountries.map((country) => (<li key={country.name}>{country.name} </li>))}
+          {filteredCountries.map((country) => (<li key={country.name}>{country.name}</li>))}
       </div>
     )
   }
@@ -63,34 +83,57 @@ const PrintCountries = ({ countries, filterString }) => {
 else{
   return <div>too many</div>
 }
-    
   
 };
 function App() {
   const [countries, setCountries] = useState([]);
   const [filterString, setNewNameFilter] = useState("");
-  const hook = () => {
+  const [wheatherData, setwheatherData] = useState([])
+ 
+  const countryHook = () => {
     console.log('effect')
     axios
-      .get('https://restcountries.eu/rest/v2/all')
-      .then(response => {
-        console.log(response.data)
-        setCountries(response.data)
-      })
+    .get('https://restcountries.eu/rest/v2/all')
+    .then(response => {
+      setCountries(response.data)
+    })
   }
   const handleFilterChange = (event) => {
     setNewNameFilter(event.target.value);
     
   };
-  useEffect(hook,[])
+  var singleCountry
+  var filteredCountries = filterCountries(countries,filterString)
+  if(filteredCountries.length===1){
+
+   singleCountry =  filteredCountries[0]
+  }
+  useEffect(countryHook,[])
+  const weatherHook = () =>{
+    if(singleCountry!=undefined){
+    axios.get("http://api.weatherapi.com/v1/current.json?key=fceec26d378b4612b15120546200908&q="+singleCountry.capital)
+    .then(response =>{
+      if(singleCountry!=undefined){
+      console.log(singleCountry.capital)
+      }
+      console.log(response.data.current.temp_c)
+      setwheatherData(response.data)
+    })
+  }
+}
+  useEffect(weatherHook,[singleCountry])
+
   return (
-  <div>
+    <div>
      <FilterInput
         filterString={filterString}
-        handleFilterChange={handleFilterChange}
-      ></FilterInput>
-      <PrintCountries countries={countries} filterString={filterString}></PrintCountries>
-  </div>)
+        handleFilterChange={handleFilterChange}>
+        </FilterInput>
+        
+      <PrintCountries filteredCountries={filteredCountries}></PrintCountries>
+      <PrintwheatherData country={singleCountry} data={wheatherData}></PrintwheatherData>
+  </div>
+  )
 }
 
 export default App;
