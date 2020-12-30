d832912bf8fec22f310948903e0001e87384e30f
kujanenj <jussi.kujanen@gmail.com>
2020-12-30 17:50:34 +0200

    9.3

--- osa9/initial/bmiCalculator.ts
+++ osa9/initial/bmiCalculator.ts
@@ -2,11 +2,17 @@ const calculateBMI = (height: number,weight: number):string =>{
     height = height /100;
     const bmi = (weight/(Math.pow(height,2)))
     if(bmi < 18.5){
+        console.log("Underweigh")
         return("Underweight")
     }
     if(bmi > 24.9){
+        console.log("OverWeight")
         return ("Overweight")
+        
     }
+    console.log("Normal")
     return "Normal"
 }
-console.log(calculateBMI(180, 74))
+const a:number = Number(process.argv[2])
+const b:number = Number(process.argv[3])
+calculateBMI(a,b)
--- osa9/initial/exerciseCalculator.ts
+++ osa9/initial/exerciseCalculator.ts
@@ -11,37 +11,55 @@ interface result {
   target: number;
   average: number;
 }
+const parseArguments = (args: Array<string>): Calculator => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  for (var i = 2; i < args.length; i++) {
+    if (isNaN(Number(args[i]))) {
+      console.log(args[i])
+      throw new Error("Provided values were not numbers!");
+    }
+    return {
+      list: args.slice(3, args.length).map(Number),
+      target: Number(args[2]),
+    };
+  }
+};
+
 const calculateAverage = (list: Array<number>, target: number): result => {
   const trainingDays: number = list.filter((value) => value > 0).length;
   const periodLength: number = list.length;
-  var sum : number = 0;
+  var sum: number = 0;
   for (var i = 0; i < list.length; i++) {
-    sum += list[i] //don't forget to add the base
+    sum += list[i]; //don't forget to add the base
   }
 
-  var average : number = sum / list.length;
-  const success : boolean = average >= target;
-  let rating : number =3
-  let description : string = "Success"; 
-  if(!success){
-      if(target-average > 1){
-          rating = 1;
-          description = "Far away"
-      }
-      else{
-          description ="Close, but not quite"
-          rating = 2
-      }
+  var average: number = sum / list.length;
+  const success: boolean = average >= target;
+  let rating: number = 3;
+  let description: string = "Success";
+  if (!success) {
+    if (target - average > 1) {
+      rating = 1;
+      description = "Far away";
+    } else {
+      description = "Close, but not quite";
+      rating = 2;
+    }
   }
 
   return {
-      periodLength,
-      trainingDays,
-      success,
-      rating,
-      description,
-      target,
-      average
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    description,
+    target,
+    average,
   };
 };
-console.log(calculateAverage([3, 0, 2, 4.5, 0, 3, 1],2))
+try {
+  const { list, target } = parseArguments(process.argv);
+  console.log(calculateAverage(list, target));
+} catch (e) {
+  console.log("Error, something bad happened, message: ", e);
+}
