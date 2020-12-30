e7731f13df15a76cec76ee2cc638ff6ddc951d3c
kujanenj <jussi.kujanen@gmail.com>
2020-12-30 18:11:41 +0200

    9.5

--- osa9/initial/bmiCalculator.ts
+++ osa9/initial/bmiCalculator.ts
@@ -1,4 +1,4 @@
-const calculateBMI = (height: number,weight: number):string =>{
+export const calculateBMI = (height: number,weight: number):string =>{
     height = height /100;
     const bmi = (weight/(Math.pow(height,2)))
     if(bmi < 18.5){
--- osa9/initial/index.ts
+++ osa9/initial/index.ts
@@ -1,6 +1,19 @@
 import express from "express";
+import { calculateBMI } from "./bmiCalculator";
 const app = express();
-
+app.get("/bmi", (req, res) => {
+  try {
+    let bmi: string = calculateBMI(
+      Number(req.query.height),
+      Number(req.query.weight)
+    );
+    res.send({
+      weight: req.query.weight,
+      height: req.query.height,
+      bmi,
+    });
+  } catch (e) {}
+});
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack");
 });
