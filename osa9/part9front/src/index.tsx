eea80c1b5dc34b2adb95f0d30ab1c9cafb7f3886
Kujanenj <44529387+Kujanenj@users.noreply.github.com>
2021-01-11 21:27:07 +0200

    9.15

--- osa9/part9front/src/index.tsx
+++ osa9/part9front/src/index.tsx
@@ -1,12 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
-interface Part {
-  name: string;
 
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+interface CoursePartBase {
+  name: string;
   exerciseCount: number;
 }
+
+interface CoursePartOne extends CoursePartBase {
+  name: "Fundamentals";
+  description: string;
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartBase, CoursePartWithDescription {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+interface CoursePartFour extends CoursePartBase, CoursePartWithDescription{
+  name: "Fourth",
+}
+interface CoursePartWithDescription extends CoursePartBase {
+  description: string;
+}
 interface PartsProps {
-  courses: Part[];
+  courses: CoursePart[];
+}
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree | CoursePartFour;
+
+interface SinglePart {
+  course: CoursePart;
 }
 
 interface HeaderProps {
@@ -16,9 +47,9 @@ const Content: React.FC<PartsProps> = ({ courses }: PartsProps) => {
   return (
     <div>
       {courses.map((course) => (
-        <p key={course.name}>
-          {course.name} {course.exerciseCount}
-        </p>
+        <div key={course.name}>
+          <Part course={course}></Part>
+        </div>
       ))}
     </div>
   );
@@ -32,22 +63,69 @@ const Total: React.FC<PartsProps> = (props) => {
   }, 0);
   return <div>{total}</div>;
 };
+const Part: React.FC<SinglePart> = (props) => {
+  switch (props.course.name) {
+    case "Fundamentals":
+      return (
+        <div>
+          <p>
+            {`${props.course.name} ${props.course.description} ${props.course.exerciseCount}`}
+          </p>
+        </div>
+      );
 
+    case "Using props to pass data":
+      return (
+        <div>
+          <p>
+            {`${props.course.name} ${props.course.groupProjectCount} ${props.course.exerciseCount}`}
+          </p>
+        </div>
+      );
+    case "Deeper type usage":
+      return (
+        <div>
+          <p>
+            {`${props.course.name} ${props.course.description} ${props.course.exerciseSubmissionLink} ${props.course.exerciseCount}`}
+          </p>
+        </div>
+      );
+      case "Fourth":
+        return (
+        <div>
+          <p>
+            {`${props.course.name} ${props.course.description}  ${props.course.exerciseCount}`}
+          </p>
+        </div>
+      );
+    default:
+      return assertNever(props.course);
+  }
+};
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is an awesome course part",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
     },
+    {
+      name: "Fourth",
+      exerciseCount: 1,
+      description: "TestDescp"
+    }
   ];
 
   return (
